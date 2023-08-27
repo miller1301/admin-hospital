481af82ab08597bcf896ef57162365768e228aee
Santiago Nivia <millernivia1301@gmail.com>
2023-08-27 13:32:19 -0500

    Sección 7

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../services/settings.service';
+
+declare function customInitFunctions(): any;
 
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
   styleUrls: ['./pages.component.scss']
 })
-export class PagesComponent {
+export class PagesComponent implements OnInit{
+
+  constructor( private settingsService: SettingsService ) {}
+
+  ngOnInit(): void {
+    customInitFunctions();
+  }
 
 }
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SidebarService } from 'src/app/services/sidebar.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
+
+  menuItems: any[];
+
+  constructor( private sidebarServices: SidebarService ) {
+    this.menuItems = this.sidebarServices.menu;
+    console.log(this.menuItems);
+    
+  }
 
 }
